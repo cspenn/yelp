0094f67222c9b088f756d1fc9da80c9cb7af8251
Christopher Penn <cspenn@gmail.com>
2018-04-07 20:47:16 -0400

    Update authentication.R

--- R/authentication.R
+++ R/authentication.R
@@ -2,5 +2,6 @@ get_access_token <- function(api_key) {
   if(api_key == "") {
     stop("You need to provide an API key")
   }
+  Sys.setenv(YELP_ACCESS_TOKEN = api_key)
+
 }
-Sys.setenv(YELP_ACCESS_TOKEN = api_key)
